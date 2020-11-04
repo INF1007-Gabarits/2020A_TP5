4f65295f79bca9062e6a5457a5cf81722eff5069
Sbelgana <44445774+Sbelgana@users.noreply.github.com>
2020-11-04 12:57:51 -0500

    Update Data_Processing.py

--- Data_Processing.py
+++ Data_Processing.py
@@ -2,39 +2,42 @@ import pandas as pd
 
 def Load_df(add_death_df, add_confirmed_df, add_recovered_df, add_summury_df):
     #TO DO: Lire les base de donnees a partir des adresse recu en parametre
+
+    
+    
     
     return ...
 
 def summuryaddcol(df, col, value):
-    #TO DO: Ajouter une colonne au DataFrame. val represente le nom de la colonne et value les valeur de la colonne
+    #TO DO: Ajouter une colonne au DataFrame
     
-    return ...
+    return ..
 
 def summuryextractcol(df, cols):
-    #TO DO: extraire les colomnes desirer de la base de donnee
+    #TO DO: extraire les colomnes desirer de la base de donnee ( une seul ligne)
     return ...
 
 def summurybycountry(df):
-    #TO DO: grouper le DataFrame par Country_Region
+    #TO DO: grouper le DataFrame par Country_Region ( une seul ligne)
     return ...
 
 def creatdictdf(death_df,confirmed_df,recovered_df):
-    #TODO: creer un disctionnaire de base de donnees
+    #TODO: creer un disctionnaire de base de donnees ( une seul ligne)
     return ...
 
 def dictdfremovecol(dict_df, cols):
-    #TO DO: Supprimer une colonne de votre dictionnaire de DataFrame
+    #TO DO: Supprimer une colonne de votre dictionnaire de DataFrame ( une seul ligne)
     return ...
 
 def dictdfbycountry(dict_df):
     #TO DO: grouper le dictionnaire de DataFrame par Country/Region et changer les colonnes en datetime 
+
     
     
-    
-    return ...
+    return dict_df
 
 def dictdfaddkey(dict_df): 
-    #TO DO: Ajouter les cles Active case et Closed Case a votre dictionnaire de DataFrame et les trier comme suit(Confirmed,Deaths,Active,Closed,Recovered)
+    #TO DO: Ajouter les cles Active case et Closed Case a votre dictionnaire de DataFrame
 
     
     
@@ -42,13 +45,14 @@ def dictdfaddkey(dict_df):
     return ...
 
 def dictdfbyday(dict_df):
-    #TO DO: grouper le dictionnaire de DataFrame par date
+    #TO DO: grouper le dictionnaire de DataFrame par date ( une seul ligne)
     return ...
 
 def basicinfsummury(summury_df):
     #TO DO: Afficher les information suivante: Somme des nombres de cas confirmée, active, fermée, mort et rétabli dans le monde.
-   
 
-
-
-
+    
+    
+    
+    
+    
